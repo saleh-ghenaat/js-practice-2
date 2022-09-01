40b4f86ee46fe46f9996de9bf049e535b8a59ee4
saleh-ghenaat <108237024+saleh-ghenaat@users.noreply.github.com>
2022-09-01 08:03:44 -0700

    upload-practice-js-2

---
+++ app.js
@@ -0,0 +1,11 @@
+function age(){
+    const age = +prompt('Enter your age')
+    if( age==19 ){
+alert(`Congratulations :) 56 ==> your age = ${age+56}`)
+    }
+    else{
+        alert('your age is not valid');
+    }
+}
+
+age()
